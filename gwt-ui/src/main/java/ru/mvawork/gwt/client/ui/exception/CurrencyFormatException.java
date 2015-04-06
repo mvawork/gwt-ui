e046a35fbe5bac8ae103cca20152e701feba2063
Vasily Menshev <mvawork@mail.ru>
2015-04-06 17:36:09 +0300

    Промежутоный результат

---
+++ gwt-ui/src/main/java/ru/mvawork/gwt/client/ui/exception/CurrencyFormatException.java
@@ -0,0 +1,17 @@
+package ru.mvawork.gwt.client.ui.exception;
+
+public class CurrencyFormatException extends IllegalArgumentException {
+
+    public CurrencyFormatException() {
+        super();
+    }
+
+    public CurrencyFormatException(String s) {
+        super(s);
+    }
+
+    static public CurrencyFormatException forInputString(String s) {
+        return new CurrencyFormatException("For input string: \"" + s + "\"");
+    }
+
+}
--- gwt-ui/src/main/java/ru/mvawork/gwt/client/ui/textbox/CurrencyBox.java
+++ gwt-ui/src/main/java/ru/mvawork/gwt/client/ui/textbox/CurrencyBox.java
@@ -4,39 +4,19 @@ import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.*;
-import com.google.gwt.regexp.shared.MatchResult;
-import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.text.shared.AbstractRenderer;
 import com.google.gwt.text.shared.Parser;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.ValueBox;
+import ru.mvawork.gwt.client.ui.exception.CurrencyFormatException;
+
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.logging.Logger;
 
 public class CurrencyBox extends ValueBox<BigDecimal> implements ChangeHandler, KeyPressHandler, KeyDownHandler {
 
-    RegExp regExp = RegExp.compile("^\\d*(\\.\\d*)?$");
-
-
     private static final Logger log = Logger.getLogger(CurrencyBox.class.getName());
-
-    public static class CurrencyFormatException extends IllegalArgumentException {
-
-        public CurrencyFormatException() {
-            super();
-        }
-
-        public CurrencyFormatException(String s) {
-            super(s);
-        }
-
-        static CurrencyFormatException forInputString(String s) {
-            return new CurrencyFormatException("For input string: \"" + s + "\"");
-        }
-
-    }
-
 
     private static String getClearInput(CharSequence text) {
         StringBuilder sb = new StringBuilder();
@@ -54,23 +34,7 @@ public class CurrencyBox extends ValueBox<BigDecimal> implements ChangeHandler, 
         public String render(BigDecimal object) {
             if (object == null)
                 return "";
-            String s = object.setScale(2, BigDecimal.ROUND_FLOOR).toPlainString();
-            int l = s.length() - 3;
-            StringBuilder sb = new StringBuilder();
-            int z = l % 3;
-            sb.append(s, 0, z);
-            while (z < l) {
-                if (z > 0)
-                    sb.append(" ");
-                sb.append(s, z, z + 3);
-                z += 3;
-            }
-            /* Добавить дробную часть */
-            s = s.substring(z+1, z+3);
-            sb.append(".");
-            sb.append(s);
-
-            return sb.toString();
+            return new ParsedText(object.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString()).getText();
         }
     }
 
@@ -79,7 +43,11 @@ public class CurrencyBox extends ValueBox<BigDecimal> implements ChangeHandler, 
         @Override
         public BigDecimal parse(CharSequence text) throws ParseException {
             try {
-                return text == null ? null : new BigDecimal(getClearInput(text));
+                if (text == null)
+                    return null;
+                BigDecimal decimal = new BigDecimal(getClearInput(text));
+                decimal.setScale(2,  BigDecimal.ROUND_HALF_UP);
+                return decimal;
             } catch (NumberFormatException e) {
                 return null;
             }
@@ -97,27 +65,42 @@ public class CurrencyBox extends ValueBox<BigDecimal> implements ChangeHandler, 
         sinkEvents(Event.ONPASTE);
     }
 
-    private boolean checkInputText(String text) {
-        log.fine(text);
-        text = getClearInput(text);
-        MatchResult matchResult = regExp.exec(text);
-        return matchResult != null && matchResult.getInput().equals(text);
-    }
-
     @Override
     public void onBrowserEvent(Event event) {
         super.onBrowserEvent(event);
         switch (event.getTypeInt()) {
             case Event.ONPASTE:
                 final String text = getText();
+                final int prevPos = getCursorPos();
                 Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
                     @Override
                     public void execute() {
-                        if (!checkInputText(getClearInput(getText())))
-                            setText(text);
+
+
+                        String s = getText();
+
+                        if (!s.isEmpty()) {
+                            int pos;
+                            log.fine(String.valueOf(prevPos));
+                            if (text.length() >= s.length()) {
+                                pos = s.length();
+                            } else {
+                                pos = prevPos + s.length() - text.length();
+                            }
+                            log.fine(s);
+                            log.fine(String.valueOf(pos));
+                            if (!trySetText(s, pos)) {
+                                setText(text);
+                                setCursorPos(prevPos);
+                            }
+                        }
                     }
                 });
                 break;
+            case Event.ONBLUR:
+                setValue(getValue());
+                break;
+
         }
     }
 
@@ -127,65 +110,86 @@ public class CurrencyBox extends ValueBox<BigDecimal> implements ChangeHandler, 
         setValue(getValue());
     }
 
-    private void setText(String text, int cursorPos) {
-        StringBuilder sb = new StringBuilder();
-        boolean allowDecimalSeparator = true;
-        char c;
-        int l = text.length();
-        if (cursorPos > l)
-            cursorPos = l;
-        if (cursorPos < 0)
-            cursorPos = 0;
+    private static class ParsedText {
+        private final int cursorPos;
+        private final String text;
 
-        int digitNum = 0, spaceNum = 0;
-        for (int i = l; i > 0; i--) {
-            c = text.charAt(i-1);
-            switch (c) {
-                case ' ':
-                    if (cursorPos >=i)
-                        cursorPos--;
-                    break;
-                case '.':
-                    if (!allowDecimalSeparator)
-                        throw CurrencyFormatException.forInputString(text);
-                    allowDecimalSeparator = false;
-
-                    for (int j = sb.length(); j > 0; j--) {
-                        if (sb.charAt(j-1) == ' ') {
-                            sb.delete(j-1, j);
-                        }
-                    }
-                    spaceNum = 0;
-                    digitNum = 0;
-                    break;
-                case '0':case '1':case '2':case '3':case '4':
-                case '5':case '6':case '7':case '8':case '9':
-                    digitNum++;
-                    if (digitNum > 3 && digitNum%3 == 1) {
-                        sb.append(' ');
-                        if (cursorPos > i) {
-                            spaceNum++;
-                        }
-                    }
-                    break;
-                default:
-                    throw CurrencyFormatException.forInputString(text);
-            }
-            if (c != ' ')
-                sb.append(c);
-
+        public ParsedText(String text) {
+            this(text, text.length());
         }
-        cursorPos += spaceNum;
-        sb.reverse();
-        setText(sb.toString());
-        setCursorPos(cursorPos);
+
+        public ParsedText(String text, int cursorPos) {
+            StringBuilder sb = new StringBuilder();
+            boolean allowDecimalSeparator = true;
+            char c;
+            int l = text.length();
+            if (cursorPos > l)
+                cursorPos = l;
+            if (cursorPos < 0)
+                cursorPos = 0;
+            int digitNum = 0, spaceNum = 0;
+            for (int i = l; i > 0; i--) {
+                c = text.charAt(i-1);
+                switch (c) {
+                    case ' ':
+                        if (cursorPos >=i)
+                            cursorPos--;
+                        break;
+                    case '.':
+                        if (!allowDecimalSeparator)
+                            throw CurrencyFormatException.forInputString(text);
+                        allowDecimalSeparator = false;
+
+                        for (int j = sb.length(); j > 0; j--) {
+                            if (sb.charAt(j-1) == ' ') {
+                                sb.delete(j-1, j);
+                            }
+                        }
+                        spaceNum = 0;
+                        digitNum = 0;
+                        break;
+                    case '0':case '1':case '2':case '3':case '4':
+                    case '5':case '6':case '7':case '8':case '9':
+                        digitNum++;
+                        if (digitNum > 3 && digitNum%3 == 1) {
+                            sb.append(' ');
+                            if (cursorPos > i) {
+                                spaceNum++;
+                            }
+                        }
+                        break;
+                    default:
+                        throw CurrencyFormatException.forInputString(text);
+                }
+                if (c != ' ')
+                    sb.append(c);
+
+            }
+            cursorPos += spaceNum;
+            sb.reverse();
+            this.text = sb.toString();
+            this.cursorPos = cursorPos;
+        }
+
+        public int getCursorPos() {
+            return cursorPos;
+        }
+
+        public String getText() {
+            return text;
+        }
+
     }
 
-    private void trySetText(String text, int cursorPos) {
+    private boolean trySetText(String text, int cursorPos) {
         try {
-            setText(text, cursorPos);
+            ParsedText parsedText = new ParsedText(text, cursorPos);
+            setText(parsedText.getText());
+            setCursorPos(parsedText.getCursorPos());
+            return true;
         } catch (CurrencyFormatException e) {
             //ToDo fire event InvalidInput
+            return false;
         }
     }
 
@@ -209,7 +213,7 @@ public class CurrencyBox extends ValueBox<BigDecimal> implements ChangeHandler, 
                 }
                 sb.append(c);
             }
-            setText(sb.toString(), pos + 1);
+            trySetText(sb.toString(), pos + 1);
         }
         if (c != 0)
             cancelKey();
@@ -221,6 +225,7 @@ public class CurrencyBox extends ValueBox<BigDecimal> implements ChangeHandler, 
         StringBuilder sb = new StringBuilder();
         NativeEvent ne = event.getNativeEvent();
         final String text = getText();
+        final int prevPos = getCursorPos();;
         switch (event.getNativeKeyCode()) {
             case KeyCodes.KEY_Z:
             case KeyCodes.KEY_Y:
@@ -228,51 +233,54 @@ public class CurrencyBox extends ValueBox<BigDecimal> implements ChangeHandler, 
                     Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
                         @Override
                         public void execute() {
-                            if (!checkInputText(getClearInput(getText())))
+                            String s = getText();
+                            if (!trySetText(s, s.length())) {
                                 setText(text);
+                                setCursorPos(prevPos);
+                            }
                         }
                     });
                     break;
                 }
             case KeyCodes.KEY_DELETE:
                 if (text != null && !text.isEmpty()) {
-                    int pos = getCursorPos();
-                    if (pos >= 0) {
-                        int l = text.length();
-                        if (pos < l) {
-                            sb.append(text, 0, pos);
-                            int s = getSelectionLength();
-                            if  (s == 0) {
-                                if (text.charAt(pos) == ' ' && pos < l - 1) {
-                                    sb.append(text, pos + 2, l);
-                                } else {
-                                    sb.append(text, pos + 1, l);
-                                }
+                    int l = text.length();
+                    if (prevPos < l) {
+                        sb.append(text, 0, prevPos);
+                        int s = getSelectionLength();
+                        if (s == 0) {
+                            if (text.charAt(prevPos) == ' ' && prevPos < l - 1) {
+                                sb.append(text, prevPos + 2, l);
                             } else {
-                                sb.append(text, pos + s, l);
+                                sb.append(text, prevPos + 1, l);
                             }
-                            trySetText(sb.toString(), pos);
-                            cancelKey();
+                        } else {
+                            sb.append(text, prevPos + s, l);
                         }
+                        trySetText(sb.toString(), prevPos);
+                        cancelKey();
                     }
                 }
+
                 break;
             case KeyCodes.KEY_BACKSPACE:
                 if (text != null && !text.isEmpty()) {
-                    int pos = getCursorPos();
-                    if (pos >= 0) {
-                        int s = getSelectionLength();
-                        int l = text.length();
-                        if (s > 0) {
-                            sb.append(text, 0, pos);
-                            sb.append(text, pos + s, l);
-                        } else {
-                            sb.append(text, 0, pos -1);
+                    int pos = prevPos;
+                    int s = getSelectionLength();
+                    int l = text.length();
+                    if (s > 0) {
+                        sb.append(text, 0, pos);
+                        sb.append(text, pos + s, l);
+                    } else {
+                        if (pos > 0) {
+                            if (pos > 1 && text.charAt(pos - 1) == ' ')
+                                pos--;
+                            sb.append(text, 0, pos - 1);
                             sb.append(text, pos, l);
                         }
-                        trySetText(sb.toString(), pos - 1);
-                        cancelKey();
                     }
+                    trySetText(sb.toString(), pos - 1);
+                    cancelKey();
                 }
                 break;
         }
